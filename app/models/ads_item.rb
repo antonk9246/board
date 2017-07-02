6d9b29981ece540f6e71db6d8f483339e243729a
antonk9246 <antonk9246@gmail.com>
2017-07-02 13:30:25 +0000

    Fix typography

--- app/models/ads_item.rb
+++ app/models/ads_item.rb
@@ -1,9 +1,9 @@
 class AdsItem < ApplicationRecord
-  validates_presence_of :title
   validates :title, presence: true, :length => {
-    :maximum   => 30}
+    :maximum   => 100}
   validates :text, :length => {
     :maximum   => 800}
+  paginates_per 50
   mount_uploaders :images, ImageUploader   
   serialize :images, JSON # If you use SQLite, add this line.
 end
