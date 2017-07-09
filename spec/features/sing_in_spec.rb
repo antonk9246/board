f42174df4ea28ec7673630ae223664404170b36f
antonk9246 <antonk9246@gmail.com>
2017-07-09 15:59:55 +0300

    dd sign in tests

--- spec/features/sing_in_spec.rb
+++ spec/features/sing_in_spec.rb
@@ -5,4 +5,22 @@ feature 'Sign in', :devise do
     signin('person@example.com', 'password')
     expect(page).to have_content 'Invalid Email or password'
   end
+
+  scenario 'user can sign in with valid credentials' do
+    user = FactoryGirl.create(:user)
+    signin(user.email, user.password)
+    expect(page).to have_content 'Signed in successfully.'
+  end
+
+  scenario 'user can not sign in with incorrect email' do
+    user = FactoryGirl.create(:user)
+    signin('invalid@email.com', user.password)
+    expect(page).to have_content 'Invalid Email or password'
+  end
+
+  scenario 'user can not sign in with incorrect password' do
+    user = FactoryGirl.create(:user)
+    signin(user.email, 'incorrect_password')
+    expect(page).to have_content 'Invalid Email or password'
+  end
 end
