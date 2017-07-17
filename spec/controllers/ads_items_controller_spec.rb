ba44ae4ee26dac49ffddf700d26ea77ce14d0f21
antonk9246 <antonk9246@gmail.com>
2017-07-17 14:47:37 +0300

    show controller spec test

--- spec/controllers/ads_items_controller_spec.rb
+++ spec/controllers/ads_items_controller_spec.rb
@@ -1,50 +1,25 @@
 require 'rails_helper'
 
-
 RSpec.describe AdsItemsController, :type => :controller do
-  describe "anonymous user" do
+  describe "index controller for guest" do
     before :each do
-      # This simulates an anonymous user
       login_with nil
     end
 
-    it "show index page" do
+    it "show index page for guest" do
       get :index
       expect(response).to have_http_status(200)
     end
   end
+  
+  describe "index controller for user" do
+    before :each do
+      login_with create( :user )
+    end
 
-  it "if user is lig in" do
-    login_with create( :user )
-    get :index
-    expect( response ).to have_http_status(200)
+    it "show index page for user" do
+      get :index
+      expect(response).to have_http_status(200)
+    end
   end
 end
-
-
-
-
-
-# RSpec.describe AdsItemsController, type: :controller do
-
-#   describe "POST #create" do
-#     context "with valid attributes" do
-#       it "create new ad" do
-#         sign_in
-#         post :create, params: { ads_item: attributes_for(:ads_item) }
-#         expect(AdsItem.count).to eq(1)
-#       end
-#     end
-#   end
-# end
-
-# it "blocks unauthenticated access" do
-#     sign_in nil
-#     get :index
-#     expect(response).to redirect_to(new_user_session_path)
-#   end
-#   it "allows authenticated access" do
-#     sign_in
-#     get :index
-#     expect(response).to be_success
-#   end
