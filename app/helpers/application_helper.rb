5f69a13ed2ae16f00a95dcbde7c3d2936fe833f6
antonk9246 <antonk9246@gmail.com>
2017-07-25 14:03:17 +0300

    Dropdown buttons for sort

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def sortable(title, column, direction)
+    direction = sort_direction == "asc" ? "desc" : "asc"
+    icon = sort_direction == 'desc' ? '↓' : '↑'
+
+    link_to ads_items_path, class: 'tiny button secondary', id: column do
+      "#{title} &nbsp; #{icon}".html_safe
+    end
+  end
 end
