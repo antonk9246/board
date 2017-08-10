eb36c88f5b8566de2dc6fa6ece0afd74cbd2c6c7
antonk9246 <antonk9246@gmail.com>
2017-08-10 10:51:13 +0300

    [Issue 11] Convert erb to slim
    https://github.com/antonk9246/board/issues/11

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   
-  get 'users', to: 'ads_items#index'
   get 'ads_items/user_page'
-  get 'ads_items/index'
-  
-
-  scope "/:locale", locale: /en|ru/ do
-    root 'ads_items#index'
+  root 'ads_items#index'
+  scope '/:locale', locale: /en|ru/ do
     get 'ads_items', to: 'ads_items#index'
     resources :ads_items
     devise_for :users
