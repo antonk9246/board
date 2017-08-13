d21f2e1a5f1c976c5d91b3c4d05477cdd37c24be
antonk9246 <antonk9246@gmail.com>
2017-08-13 12:29:30 +0300

    Migrate from SQLite to PostgreS

--- Gemfile
+++ Gemfile
@@ -18,7 +18,7 @@ gem 'slim-rails'
 gem "paperclip"
 
 gem 'pundit'
-gem 'sqlite3'
+gem 'pg'
 gem 'puma', '~> 3.0'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170811192900) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "ads_items", force: :cascade do |t|
     t.datetime "approval_date"
     t.text     "title"
@@ -22,8 +25,8 @@ ActiveRecord::Schema.define(version: 20170811192900) do
     t.integer  "user_id"
     t.boolean  "approved"
     t.integer  "category_id"
-    t.index ["category_id"], name: "index_ads_items_on_category_id"
-    t.index ["user_id"], name: "index_ads_items_on_user_id"
+    t.index ["category_id"], name: "index_ads_items_on_category_id", using: :btree
+    t.index ["user_id"], name: "index_ads_items_on_user_id", using: :btree
   end
 
   create_table "categories", force: :cascade do |t|
@@ -51,7 +54,8 @@ ActiveRecord::Schema.define(version: 20170811192900) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
+
 end
