0e359520e5a85fa0ee73f8e55772267487170ab4
antonk9246 <antonk9246@gmail.com>
2017-08-17 20:52:09 +0300

    [Issue 15] Delete categories
    https://github.com/antonk9246/board/issues/15

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Category do
   permit_params :en, :ru
+
+  member_action :destroy do
+    @category = Category.find(params[:id])
+    if AdsItem.where(:category_id => @category.id).count == 0
+      @category.destroy
+      redirect_to admin_categories_path(locale), notice: (t 'categories.delete').to_s
+    else
+      redirect_to admin_categories_path(locale), notice: (t 'categories.not_delete').to_s
+    end
+  end
 end
